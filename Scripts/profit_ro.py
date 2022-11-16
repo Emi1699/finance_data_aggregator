34fc4bf39697ad5e9c05354322ea18488afcb231
Emi1699 <emanuelbuliga16@gmail.com>
2022-11-16 17:16:22 +0200

    successfully retrieved new from profit.ro

--- Scripts/profit_ro.py
+++ Scripts/profit_ro.py
@@ -1,35 +1,40 @@
-import helpers
-from datetime import date
 import os
+from datetime import date
+import helpers
 from news import News
 
-def get_latest_news(sourceLink = "https://www.zf.ro/"):
+
+def pause():
+    programPause = input("\nPress the <ENTER> key to continue...\n")
+
+def get_latest_news(sourceLink = "https://www.profit.ro/toate"):
     source = helpers.get_source(sourceLink)
     news = {}
     id = 1
 
+    # find path to output file
     absolute_path = os.path.dirname(__file__)
     relative_path = "../News/profit_ro.txt"
     output_file = os.path.join(absolute_path, relative_path) 
 
-    latestNews_wrapper = source.select(".clear.latest-wrapper")
+    latestNews_wrapper = source.select("ul.articles")
 
     for wrapper in latestNews_wrapper:
         for list_el in wrapper.find_all("li"):
+            if not list_el.has_attr("id"):
 
-            newsDate = str(date.today()) + " @ " + list_el.find("small").text
-            newsLink = sourceLink + list_el.find("a")['href']
-            newsText = list_el.find("a").text
+                newsDate = list_el.select("div.col-xs-12.col-sm-8.col-md-9")[0].select("div")[0].text
+                newsText = list_el.select("div.col-xs-12.col-sm-8.col-md-9")[0].select("a")[0].text
+                newsLink = "https://www.profit.ro" + list_el.select("div.col-xs-12.col-sm-8.col-md-9")[0].select("a")[0]['href']
 
-            newsPiece = News(newsDate, newsText, newsLink, id)
-            news[newsPiece.id] = (newsPiece.date, newsPiece.text, newsPiece.link)
+                newsPiece = News(newsDate, newsText, newsLink, id)
+                news[newsPiece.id] = (newsPiece.date, newsPiece.text, newsPiece.link)
 
-            id += 1
+                id += 1
 
     with open(output_file, 'w', encoding='utf-8') as f:
         for article in news.values():
             f.write(str(article)+ "\n")
-
     f.close()
 
     # helpers.add_news_to_excel(news)
