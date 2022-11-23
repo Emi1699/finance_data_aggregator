1e9b07f59ceb88d79882032d4006c7a3dc37d608
Emi1699 <emanuelbuliga16@gmail.com>
2022-11-23 20:32:25 +0200

    added comments

--- Scripts/file_system.py
+++ Scripts/file_system.py
@@ -62,17 +62,19 @@ def get_path_to_file(name, file, dir_type):
 
     return output_file
 
+# overwrite
 def write_to_file(fl, txt, finish_text = "Content has been written to file."):
     if fl == "ziarul_financiar" or fl == "wall_street" or fl == "profit_ro":
         output_file = get_path_to_file(fl, __file__, "nbs")
     else:
         output_file = get_path_to_file(fl, __file__, "nbt")
 
-    with open(output_file, 'a', encoding="utf-8") as f:
+    with open(output_file, 'w', encoding="utf-8") as f:
         f.write(txt + "\n")
 
     print(f"{finish_text}")
 
+# append if not already in file
 def write_ticker_to_file(fl, txt, finish_text = "Content has been written to file."):
     if fl == "ziarul_financiar" or fl == "wall_street" or fl == "profit_ro":
         output_file = get_path_to_file(fl, __file__, "nbs")
