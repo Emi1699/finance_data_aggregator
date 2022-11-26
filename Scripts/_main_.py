7522afcf014fdae0d1356457d71e7803df392895
Emi1699 <emanuelbuliga16@gmail.com>
2022-11-26 19:33:10 +0200

    refactoring due to changing retrieval_module's name (news)

--- Scripts/_main_.py
+++ Scripts/_main_.py
@@ -1,21 +1,20 @@
-import retrieval_module as rm
+import news_retrieval_module as rm
 import time
 import requests
 import helpers
 
 
 def main():
-    retrieval_module = rm.RetrievalModule()
+    news_retrieval_module = rm.NewsRetrievalModule()
     pause_len = 300
 
     while True:
 
-        retrieval_module.get_latest_news_ziarul_financiar()
-        retrieval_module.get_latest_news_wall_street_ro()
-        retrieval_module.get_latest_news_profit_ro()
+        news_retrieval_module.get_latest_news_ziarul_financiar()
+        news_retrieval_module.get_latest_news_wall_street_ro()
+        news_retrieval_module.get_latest_news_profit_ro()
 
         helpers.pause(pause_len)
 
 if __name__ == "__main__":
     main()
-
