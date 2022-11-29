300a3224694d14baf46fa62141a4f12d93a98051
Emanuel Buliga <emanuel.buliga@phipartners.com>
2022-11-29 17:33:54 +0200

    here we will add the curl headers for the 'trading' section on BVB

---
+++ Scripts/bvb_curl_headers_trading.py
@@ -0,0 +1,82 @@
+from companies import Company
+
+def get_curl_params(company):
+    cookies, headers, params, data = None, None, None, None
+
+    if company == Company.SOCIETATEA_ENERGETICA_ELECTRICA:
+        pass
+
+    elif company == Company.FONDUL_PROPRIETATEA:
+        pass
+
+    elif company == Company.OMV_PETROM:
+        pass
+
+    elif company == Company.BANCA_TRANSILVANIA:
+        pass
+
+    elif company == Company.TRANSPORT_TRADE_SERVICES:
+        pass
+
+    elif company == Company.CNTEE_TRANSELECTRICA:
+        pass
+
+    elif company == Company.SNTGN_TRANSGAZ:
+        pass
+
+    elif company == Company.SNGN_ROMGAZ:
+        pass
+
+    elif company == Company.GROUP_SOCIETE_GENERALE:
+        pass
+
+    elif company == Company.EVERGENT_INVESTMENTS:
+        pass
+
+    elif company == Company.ERSTE_GROUP_BANK:
+        pass
+
+    elif company == Company.PURCARI_WINERIES:
+        pass
+
+    elif company == Company.MEDLIFE:
+        pass
+
+    elif company == Company.NUCLEARELECTRICA:
+        pass
+
+    elif company == Company.SPHERA_FRANCHISE_GROUP:
+        pass
+
+    elif company == Company.ROMCARBON:
+        pass
+
+    elif company == Company.AQUILA_PART_PROD:
+        pass
+
+    elif company == Company.TERAPLAST:
+        pass
+
+    elif company == Company.CONPET:
+        pass
+
+    elif company == Company.TRANSILVANIA_INVESTMENTS_ALLIANCE:
+        pass
+
+    elif company == Company.SIMTEL_TEAM:
+        pass
+
+    elif company == Company.NOROFERT:
+        pass
+
+    elif company == Company.SAFETECH_INNOVATIONS:
+        pass
+
+    elif company == Company.AROBS_TRANSILVANIA_SOFTWARE:
+        pass
+
+    elif company == Company.AGROSERV_MARIUTA:
+        pass
+
+
+    return [cookies, headers, params, data]
