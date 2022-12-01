5a13092c80152249a08fb84c880cfc2f058d3a51
Emi1699 <emanuelbuliga16@gmail.com>
2022-12-01 18:28:32 +0200

    refactoring

--- Scripts/keyword_search.py
+++ Scripts/keyword_search.py
@@ -2,7 +2,7 @@ import helpers
 import requests
 import re
 from File_System import file_system
-from File_System.dir_path import DirPath as dp
+from File_System.dir_path import DirPath as dir_path
 
 running = True
 testing = not running
@@ -26,7 +26,7 @@ def sort_all_news_by_tickers():
 def sort_news_by_tickers_from(source):
     print(f"\nSorting news from {source} by ticker...\n")
 
-    with open(file_system.get_path_to_file(source, __file__, dp.NEWS_BY_SOURCE), 'r', encoding="utf-8") as file:
+    with open(file_system.get_path_to_file(source, __file__, dir_path.NEWS_BY_SOURCE), 'r', encoding="utf-8") as file:
 
         if source == "ziarul_financiar.txt":
             link = 1
@@ -79,6 +79,9 @@ def sort_news_by_tickers_from(source):
                     print(f"> couldn't read tickers from page {link}")
 
                 link += 1
+
+        elif source == "profit_ro.txt":
+            print(" WORK IN PROGRESS ... ")
             
 # match a single ticker with the text in the news
 def ticker_in_news(ticker, news_text):
@@ -96,103 +99,103 @@ def check_all_tickers(source_txt, link_to_source, finish_message):
     found = False
 
     if tickers_in_news(["societatea energetica electrica", "electrica s.a.", "electrica sa"], source_txt):
-        file_system.write_ticker_to_file("EL_SOCIETATEA ENERGETICA ELECTRICA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("EL_SOCIETATEA ENERGETICA ELECTRICA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["fondul proprietatea"], source_txt):
-        file_system.write_ticker_to_file("FP_FONDUL PROPRIETATEA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("FP_FONDUL PROPRIETATEA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["omv", "petrom ", "snp", " petrom"], source_txt):
-        file_system.write_ticker_to_file("SNP_OMV PETROM", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SNP_OMV PETROM", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["banca transilvania", "tlv"], source_txt):
-        file_system.write_ticker_to_file("TLV_BANCA TRANSILVANIA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TLV_BANCA TRANSILVANIA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news([" tts ", "transport trade services"], source_txt):
-        file_system.write_ticker_to_file("TTS_TRANSPORT TRADE SERVICES", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TTS_TRANSPORT TRADE SERVICES", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news([" tel ", "transelectrica"], source_txt):
-        file_system.write_ticker_to_file("TEL_TRANSELECTRICA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TEL_TRANSELECTRICA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["tgn", "transgaz"], source_txt):
-        file_system.write_ticker_to_file("TGN_TRANSGAZ", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TGN_TRANSGAZ", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
     
     if tickers_in_news(["sng", "romgaz"], source_txt):
-        file_system.write_ticker_to_file("SNG_ROMGAZ", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SNG_ROMGAZ", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["brd", "groupe societe generale"], source_txt):
-        file_system.write_ticker_to_file("BRD_GROUPE SOCIETE GENERALE", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("BRD_GROUPE SOCIETE GENERALE", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news([" ever ", "evergent investments", "evergent"], source_txt):
-        file_system.write_ticker_to_file("EVER_EVERGENT INVESTMENTS", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("EVER_EVERGENT INVESTMENTS", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["erste group bank", "erste group", "erste"], source_txt):
-        file_system.write_ticker_to_file("EBS_ERSTE GROUP BANK AG", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("EBS_ERSTE GROUP BANK AG", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["purcari wineries", "purcari"], source_txt):
-        file_system.write_ticker_to_file("WINE_PURCARI WINERIES PUBLIC COMPANY LIMITED", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("WINE_PURCARI WINERIES PUBLIC COMPANY LIMITED", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["medlife"], source_txt):
-        file_system.write_ticker_to_file("M_MEDLIFE", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("M_MEDLIFE", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["snn", "nuclearelectrica"], source_txt):
-        file_system.write_ticker_to_file("SNN_NUCLEARELECTRICA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SNN_NUCLEARELECTRICA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["sfg", "sphera franchise group"], source_txt):
-        file_system.write_ticker_to_file("SFG_SPHERA FRANCHISE GROUP", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SFG_SPHERA FRANCHISE GROUP", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
     
     if tickers_in_news([" roce ", "romcarbon"], source_txt):
-        file_system.write_ticker_to_file("ROCE_ROMCARBON", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("ROCE_ROMCARBON", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["aquila"], source_txt):
-        file_system.write_ticker_to_file("AQ_AQUILA PART PROD", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("AQ_AQUILA PART PROD", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["trp", "teraplast"], source_txt):
-        file_system.write_ticker_to_file("TRP_TERAPLAST", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TRP_TERAPLAST", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["conpet"], source_txt):
-        file_system.write_ticker_to_file("COTE_CONPET", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("COTE_CONPET", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news([" transi ", "transilvania investments"], source_txt):
-        file_system.write_ticker_to_file("TRANSI_TRANSILVANIA INVESTMENTS ALLIANCE", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("TRANSI_TRANSILVANIA INVESTMENTS ALLIANCE", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["smtl", "simtel"], source_txt):
-        file_system.write_ticker_to_file("SMTL_SIMTEL TEAM", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SMTL_SIMTEL TEAM", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["nrf", "norofert"], source_txt):
-        file_system.write_ticker_to_file("NRF_NOROFERT", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("NRF_NOROFERT", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["safetech"], source_txt):
-        file_system.write_ticker_to_file("SAFE_SAFETECH INNOVATIONS", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("SAFE_SAFETECH INNOVATIONS", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["arobs", "arobs transilvania"], source_txt):
-        file_system.write_ticker_to_file("AROBS_AROBS TRANSILVANIA SOFTWARE", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("AROBS_AROBS TRANSILVANIA SOFTWARE", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     if tickers_in_news(["agroserv"], source_txt):
-        file_system.write_ticker_to_file("MILK_AGROSERV MARIUTA", link_to_source, dp.NEWS_BY_TICKER, finish_message)
+        file_system.write_ticker_to_file("MILK_AGROSERV MARIUTA", link_to_source, dir_path.NEWS_BY_TICKER, finish_message)
         found = True
 
     return found
