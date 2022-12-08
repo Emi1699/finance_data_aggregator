eda060199bbb6b883f0285ee2f8f5c6446c960b1
Emi1699 <emanuelbuliga16@gmail.com>
2022-12-08 23:41:01 +0200

    added paths to all directories in the market data and news section

--- Scripts/dir_path.py
+++ Scripts/dir_path.py
@@ -3,12 +3,19 @@ from enum import Enum
 class DirPath(Enum):
     NEWS_BY_SOURCE = f"../Market Data and News/News/News by Source/"
     NEWS_BY_TICKER = f"../Market Data and News/News/News by Ticker/"
+
     BVB_FINANCIALS = f"../Market Data and News/Market Data/Financials/"
+    BVB_FINANCIALS_FINANCIAL_CALENDAR = f"../Market Data and News/Market Data/Financials/Financial Calendar/"
+    BVB_FINANCIALS_ANNUAL_FINANCIAL_INFORMATION = f"../Market Data and News/Market Data/Financials/Annual Financial Information/"
+
+    BVB_TRADING = f"../Market Data and News/Market Data/Trading/"
     BVB_TRADING_PERFORMANCE = f"../Market Data and News/Market Data/Trading/Performance/"
     BVB_TRADING_HISTORY = f"../Market Data and News/Market Data/Trading/History/"
+
+    BVB_OVERVIEW = f"../Market Data and News/Market Data/Overview/"
     BVB_OVERVIEW_PRICES = f"../Market Data and News/Market Data/Overview/Prices/"
     BVB_OVERVIEW_INDICATORS = f"../Market Data and News/Market Data/Overview/Indicators/"
     BVB_OVERVIEW_ISSUE_INFO = f"../Market Data and News/Market Data/Overview/Issue Info/"
-    BVB_OVERVIEW = f"../Market Data and News/Market Data/Overview/"
+
     TEST_FILES = f"../Test Files/"
     
